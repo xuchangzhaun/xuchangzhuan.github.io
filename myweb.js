553de79ab55ccde0219939c1b74dad6c69d804f4
xuchangzhuan <920372432@qq.com>
2017-07-25 20:16:18 +0800

    shang

---
+++ myweb.js
@@ -0,0 +1,26 @@
+function submitTryit()
+{
+var t=document.getElementById("TestCode").value;
+//
+t=t.replace(/=/gi,"w3equalsign");
+t=t.replace(/script/gi,"w3scrw3ipttag");
+
+document.getElementById("code").value=t;
+
+document.getElementById("tryitform").action="v.asp";
+
+validateForm();
+
+document.getElementById("tryitform").submit();
+}
+
+function validateForm()
+{
+var code=document.getElementById("code").value;
+if (code.length>5000)
+	{
+	document.getElementById("code").value="<h1>Error</h1>";
+	}
+}
+
+}
